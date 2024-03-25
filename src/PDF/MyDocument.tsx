c572cf54d0046870720979ab82b428fbb5f10144
Daniel Quintero <daniel0725q@gmail.com>
2024-03-25 18:24:27 -0500

    Completed form functionality

--- src/PDF/MyDocument.tsx
+++ src/PDF/MyDocument.tsx
@@ -1,101 +1,70 @@
-import {
-  Document,
-  Text,
-  Page,
-  StyleSheet,
-  Image,
-  View
-} from '@react-pdf/renderer';
+import React from 'react';
+import { Document, Page, View, Text, StyleSheet, PDFDownloadLink, Image } from '@react-pdf/renderer';
 
 const styles = StyleSheet.create({
-page: {
-  backgroundColor: '#fff',
-  padding: 5,
-},
-title: {
-  fontSize: 13,
-  textAlign: 'center',
-  fontWeight: 'bold',
-  
-},
-title_bordered: {
-  fontSize: 13,
-  textAlign: 'center',
-  fontWeight: 'bold',
-  borderBottom: '1px solid black',
-  marginHorizontal: 10
-},
-section: {
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-  padding: '5px 20px 5px 20px'
-},
-parragraph: {
-  fontSize: 12,
-  textAlign: 'center',
-  lineHeight: 1.5,
-  margin: 7,
-},
-pageNumber: {
-  position: 'absolute',
-  fontSize: 12,
-  bottom: 30,
-  left: 0,
-  right: 0,
-  textAlign: 'center',
-  color: 'white',
-}
+  page: {
+    flexDirection: 'column'
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  },
+  text: {
+    fontSize: 12,
+    fontFamily: 'Times-Roman',
+  },
 });
 
-interface IPDF {
-  content: any
+interface PDFGeneratorProps {
+  content: any;
+  fileName: string;
 }
 
-export const PDF = (props: IPDF) =>  {
-  return (
-    <Document>
-      <Page 
-        size={{ width: 240, height: 540 }}
-        style={styles.page}>
-        <Text style={styles.parragraph}>Fecha de emición</Text>
-        <Text style={styles.title_bordered}>"FECHA" </Text>
-        <Text style={styles.parragraph}> Tu número de radicación es: </Text>
-        <Text style={styles.title}>
-          Título
-        </Text>
-        <Text style={styles.parragraph}>
-          Hola, aquí te atenderán
-        </Text>
-        <Text style={styles.parragraph}>
-          Tipo de servicio: completo
-        </Text>
-        <Text style={styles.parragraph}>
-          Ips: Saman
-        </Text>
-        <Text style={styles.title}>
-          Sede: SURA
-        </Text>
-        <Text style={styles.title}>
-          Hoy
-        </Text>
-        <Text style={styles.title}>
-          123.128.7.6
-        </Text>
-        <Text style={styles.title_bordered}>
-          1234567
-        </Text>
-        <Text style={styles.title}>
-          Gracias por usar
-        </Text>
-      <Text style={styles.title_bordered}>
-          puntofacil.coosalud.com
-        </Text>
+class PDFGenerator extends React.Component<PDFGeneratorProps> {
+  parse(content: any) {
+    const elements: any[] = [];
+    for (const key in content) {
+      const el = content[key];
+      if (Array.isArray(el)) {
+        el.forEach((v: any) => {
+          if (v.value.includes("<ul>")) {
+            const firstIndex = v.value.indexOf("<ul>")
+            const lastIndex = v.value.indexOf("</ul>")
+            console.log(v.value.substring(firstIndex + 4, lastIndex));
+          }
 
-      </Page>
-    </Document>
-  );
+          if (v.value.includes("<img")) {
+            const firstIndex = v.value.indexOf("<img src")
+            const lastIndex = v.value.indexOf(">", firstIndex)
+            const src =v.value.substring(firstIndex + 9, lastIndex);
+            const img = <Image src={src}></Image>;
+            elements.push(img);
+          }
+        })
+      }
+    }
+    return elements;
+  }
 
+    render() {
+      const { content, fileName } = this.props;
+      const element = (
+        <Document>
+            <Page size="A4" style={styles.page}>
+                {this.parse(content)}
+            </Page>
+        </Document>
+    );
+
+        return (
+            <div>
+                <PDFDownloadLink document={element} fileName={fileName}>
+                  Download
+                </PDFDownloadLink>
+            </div>
+        );
+    }
 }
 
-export default PDF;
+export default PDFGenerator;
