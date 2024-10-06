dfa427e01a50654ef326b23196516a1bf698a7ac
isaac jimenez <isaacj32@hotmail.com>
2024-10-05 23:17:41 -0500

    Codigo con  error

--- src/FormCopy/FormCopy.tsx
+++ src/FormCopy/FormCopy.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, Select, Space } from 'antd'
+import { Button, Input, Select } from 'antd'
 import React, { useEffect, useState } from 'react';
 import { Option } from 'antd/es/mentions';
 import { useNavigate, useParams } from 'react-router-dom';
@@ -20,7 +20,8 @@ function FormCopy() {
     version: version,
     code: code,
     type: type,
-    id: id
+    id: id,
+    sections: []
   });
 
   useEffect(() => {
@@ -37,29 +38,50 @@ function FormCopy() {
       setVersion(r.version);
       setCode(r.code);
       setType(r.type);
+      setFormData((prevData: any) => ({
+        ...prevData,
+        sections: r.form.sections || []
+      }));
     });
-  }, [])
+  }, [id, sessionStorageUser.token]);
+
+  const addSection = () => {
+    setFormData({
+      ...formData,
+      sections: [...formData.sections, {}]
+    });
+  }
+
+  const removeSection = (index: number) => {
+    const updatedSections = [...formData.sections];
+    updatedSections.splice(index, 1);
+    setFormData({
+      ...formData,
+      sections: updatedSections
+    });
+  }
 
   const generateJSON = () => {
     formData.title = title;
     formData.version = version;
     formData.code = code;
     formData.type = type;
+    formData.sections = [formData.sections];
     createFormTemplate();
   }
 
   const createFormTemplate = () => {
-    if (formData.title == '') {
-      alert('Ingrese un título')
-      return
+    if (formData.title === '') {
+      alert('Ingrese un título');
+      return;
     }
-    if (formData.version == '') {
-      alert('Ingrese una versión')
-      return
+    if (formData.version === '') {
+      alert('Ingrese una versión');
+      return;
     }
-    if (formData.code == '') {
-      alert('Ingrese un código')
-      return
+    if (formData.code === '') {
+      alert('Ingrese un código');
+      return;
     }
     fetch(`${REACT_APP_API_ENDPOINT}/form-templates/${id}/copy`, {
       method: 'POST',
@@ -73,63 +95,105 @@ function FormCopy() {
         version: formData.version,
         code: formData.code,
         type: formData.type,
+        form: {
+          sections: formData.sections
+        }
       }),
     })
       .then((r) => r.json())
       .then((r) => {
-        navigate('/forms')
-      })
-    }
+        navigate('/forms');
+      });
+  }
 
   return (
-        <div style={{textAlign: 'center'}}>
-          <h1>Copiar formulario</h1>
-          <div style={{width: '20%', marginLeft: '40%'}}>
-            <label>Título:</label>
+    <div style={{ textAlign: 'center' }}>
+      <h1>Copiar formulario</h1>
+      <div style={{ width: '20%', marginLeft: '40%' }}>
+        <label>Título:</label>
+        <Input
+          type="text"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <br />
+        <label>Versión:</label>
+        <Input
+          type="text"
+          name="version"
+          value={version}
+          onChange={(e) => setVersion(e.target.value)}
+        />
+        <br />
+        <label>Código:</label>
+        <Input
+          type="text"
+          name="code"
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+        />
+        <br />
+        <label>Tipo:</label>
+        <Select
+          value={type}
+          onChange={(value) => setType(value)}
+        >
+          <Option value="1">ISO</Option>
+          <Option value="2">SST</Option>
+          <Option value="3">Documentación</Option>
+          <Option value="4">Mapa de Procesos</Option>
+          <Option value="5">Políticas</Option>
+          <Option value="6">Normas/Leyes</Option>
+          <Option value="7">Matriz de Riesgos</Option>
+          <Option value="8">Auditoría</Option>
+          <Option value="9">Evaluación de Desempeño</Option>
+        </Select>
+        <br />
+        <h2>Secciones</h2>
+        {formData.sections.map((section: any, index: any) => (
+          <div key={index}>
+            <h3>Sección {index + 1}</h3>
+            <label>Id:</label>
             <Input
-                type="text"
-                name="title"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
+              type="text"
+              value={section.id || ''}
+              onChange={(e: any) => {
+                const updatedSections = [...formData.sections];
+                updatedSections[index] = { ...updatedSections[index], id: e.target.value };
+                setFormData({ ...formData, sections: updatedSections });
+              }}
             />
             <br />
-            <label>Versión:</label>
+            <label>Nombre:</label>
             <Input
-                type="text"
-                name="version"
-                value={version}
-                onChange={(e) => setVersion(e.target.value)}
+              type="text"
+              value={section.name || ''}
+              onChange={(e) => {
+                const updatedSections = [...formData.sections];
+                updatedSections[index] = { ...updatedSections[index], name: e.target.value };
+                setFormData({ ...formData, sections: updatedSections });
+              }}
             />
             <br />
-            <label>Código:</label>
+            <label>Botones:</label>
             <Input
-                type="text"
-                name="code"
-                value={code}
-                onChange={(e) => setCode(e.target.value)}
+              type="text"
+              value={section.buttons ? section.buttons.join(',') : ''}
+              onChange={(e) => {
+                const updatedSections = [...formData.sections];
+                updatedSections[index] = { ...updatedSections[index], buttons: e.currentTarget.value.split(',') };
+                setFormData({ ...formData, sections: updatedSections });
+              }}
             />
-            <br />
-            <label>Tipo:</label>
-            <Select
-                value={type}
-                onChange={(value) => setType(value)}
-            >
-                <Option value="1">ISO</Option>
-                <Option value="2">SST</Option>
-                <Option value="3">Documentación</Option>
-                <Option value="4">Mapa de Procesos</Option>
-                <Option value="5">Políticas</Option>
-                <Option value="6">Normas/Leyes</Option>
-                <Option value="7">Matriz de Riesgos</Option>
-                <Option value="8">Auditoría</Option>
-                <Option value="9">Evaluación de Desempeño</Option>
-            </Select>
-            <br />
-            <Button onClick={generateJSON} type="primary" style={{marginTop: '10px'}}>Guardar formulario</Button>
-            </div>
-        </div>
-      );
-    }
-
+            <button onClick={() => removeSection(index)}>Borrar sección</button>
+          </div>
+        ))}
+        <Button onClick={addSection} type="default">Añadir sección</Button>
+        <Button onClick={generateJSON} type="primary" style={{ marginTop: '10px' }}>Guardar formulario</Button>
+      </div>
+    </div>
+  );
+}
 
 export default FormCopy;
