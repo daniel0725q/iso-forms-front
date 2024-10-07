9dde1e24887515a3ed7e6d7064cf7abe74095737
isaac jimenez <isaacj32@hotmail.com>
2024-10-07 02:37:31 -0500

    Mejorar separación de ISO/BASC/SST Fixes #15

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,58 +1,86 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import './Home.css'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Home.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHelmetSafety } from '@fortawesome/free-solid-svg-icons';
 
-interface HomeProps { 
-    loggedIn: boolean;
-    email: string;
-    setLoggedIn: any;
+interface HomeProps {
+  loggedIn: boolean;
+  email: string;
+  setLoggedIn: any;
 }
 
 const Home = (props: HomeProps) => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const onButtonClick = () => {
     if (props.loggedIn) {
-      localStorage.removeItem('user')
-      props.setLoggedIn(false)
+      localStorage.removeItem('user');
+      props.setLoggedIn(false);
     } else {
-      navigate('/login')
+      navigate('/login');
     }
-  }
+  };
 
   return (
-    <div>
-      <div className="mainContainer">
-        <div className={'titleContainer'}>
-          <img src='logogs.png' height={80}></img>
-        </div>
-        <div>
-          <b>Bienvenido</b>
-          <p>
+    <div className="mainContainer">
+      <div className={'titleContainer'}>
+        <img src='logogs.png' height={80} alt="Logo GS" />
+      </div>
+      <div className="welcomeContainer">
+        <b>Bienvenido</b>
+        <p>
           Esta es la página de inicio. Para continuar, elige una de las opciones de nuestro menú lateral o inicia sesión.
-          </p>
+        </p>
+      </div>
+      <div className="imageGrid">
+        <div className="imageItem">
+          <img
+            src='logoiso.gif'
+            height={200}
+            width={230}
+            alt="Logo ISO"
+            onClick={() => {
+              navigate('/forms?formId=1');
+            }}
+          />
         </div>
-        <div>
-          <img src='logoiso.gif' height={200} width={200} onClick={() => {
-            navigate('/forms?formId=1')
-          }}></img>
-          <img src='ods.png' height={200} width={300} onClick={() => {
-            alert("Disponible próximamente")
-          }}></img>
+        <div className="imageItem">
+          <img
+            src='ods.png'
+            height={200}
+            width={300}
+            alt="ODS Logo"
+            onClick={() => {
+              alert("Disponible próximamente");
+            }}
+          />
         </div>
-        <div>
-          <img src='basc.png' height={250} width={250} onClick={() => {
-            alert("Disponible próximamente")
-          }}></img>
-          <img src='logosst.png' height={250} width={250} onClick={() => {
-            navigate('/forms?formId=2')
-          }}></img>  
+        <div className="imageItem">
+          <img
+            src='basc.png'
+            height={250}
+            width={250}
+            alt="Logo BASC"
+            onClick={() => {
+              alert("Disponible próximamente");
+            }}
+          />
+        </div>
+        <div className="imageItem">
+          <img
+            src='logosst.png'
+            height={250}
+            width={250}
+            alt="Logo SST"
+            onClick={() => {
+              navigate('/forms?formId=2');
+            }}
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
